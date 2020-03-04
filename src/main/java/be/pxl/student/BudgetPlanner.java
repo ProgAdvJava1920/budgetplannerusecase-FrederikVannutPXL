dd0866b1cda7167c6519de69e64738937f5c97db
FrederikVannutPXL <frederik.vannut@student.pxl.be>
2020-03-04 15:49:09 +0100

    AccountDOA + docker-compose.yml + BudgetPlanner.sql

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -3,11 +3,14 @@ package be.pxl.student;
 import be.pxl.student.entity.Account;
 import be.pxl.student.util.BudgetPlannerImporter;
 
+import java.sql.Connection;
+
 public class BudgetPlanner {
     public static void main(String[] args) {
         BudgetPlannerImporter importer = new BudgetPlannerImporter();
         Account account = importer.readFile("src\\main\\resources\\account_payments.csv");
         System.out.println(account);
+
     }
 
 }
