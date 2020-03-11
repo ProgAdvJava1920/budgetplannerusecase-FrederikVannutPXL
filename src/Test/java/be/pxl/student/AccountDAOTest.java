fea39dc3dc4abfa80b9851a11a59ca184b739c8b
FrederikVannutPXL <frederik.vannut@student.pxl.be>
2020-03-11 14:45:42 +0100

    TestAccountRead

--- src/Test/java/be/pxl/student/AccountDAOTest.java
+++ src/Test/java/be/pxl/student/AccountDAOTest.java
@@ -5,18 +5,25 @@ import be.pxl.student.entity.Account;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountDAOTest {
+    AccountDAO accountDAO = new AccountDAO("jdbc:mysql://localhost:3306/budgetplanner?useSSL=false", "root", "admin");
     @Test
     public void testAccountInsert() {
         Account account = new Account();
         account.setIBAN("testIBAN");
         account.setName("testName");
 
-        AccountDAO accountDAO = new AccountDAO("jdbc:mysql://localhost:3306/budgetplanner?useSSL=false", "root", "admin");
         Account accountInserted = accountDAO.createAccount(account);
         //assertEquals(account,accountInserted);
         assertNotEquals(0, accountInserted.getId());
         System.out.println(accountInserted.getId());
+    }
+
+    @Test
+    public void testAccountRead() {
+        Account accountRead = accountDAO.readAccount(1);
+        assertEquals(accountRead.getName(), "testName");
     }
 }
